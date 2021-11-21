5ec667215daa797ee379418b138192c27c4f07a3
julie0919 <julie.lee0919@gmail.com>
2021-11-22 01:18:11 +0900

    accounts_url_updated

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('login/', obtain_jwt_token),
     path('<int:user_pk>/', views.takename),
     path('<str:username>/', views.profile, name='profile'),
-    path('<str:username>/myMovie/', views.my_movie, name='my_movie'),
+    path('<int:user_pk>/myMovie/', views.my_movie, name='my_movie'),
     path('<int:user_pk>/myArticle/', views.my_article, name='my_article'),
-    path('<str:username>/myComment/', views.my_comment, name='my_comment'),
+    path('<int:user_pk>/myComment/', views.my_comment, name='my_comment'),
     path('follow/<str:username>/', views.follow, name='follow'),
     # path('api-token-auth/', obtain_jwt_token),
 ]
