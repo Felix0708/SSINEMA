d1aa64d764786c5c1de22c1cc6dc1fc144042784
julie0919 <julie.lee0919@gmail.com>
2021-11-24 14:22:21 +0900

    comments_user_updated

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -13,4 +13,4 @@ class UserDetailSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username',)
