be08ce8f6ac798319e413d17ee8391656f407c6f
LEE DONG CHUL <chuls0708@gmail.com>
2021-11-25 22:52:22 +0900

    deploy

--- final_pjt_back/accounts/models.py
+++ final_pjt_back/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from imagekit.models import ProcessedImageField
-from imagekit.processors import ResizeToFill
+# from imagekit.models import ProcessedImageField
+# from imagekit.processors import ResizeToFill
 
 
 class User(AbstractUser):
